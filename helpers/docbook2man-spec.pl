eb4e62fdd49c8d98a3c0c00e61b151873fa7744a
Eric Gallager <egall@gwmail.gwu.edu>
2021-05-26 19:47:21 -0400

    some more changes from MacPorts
    these were in the form of reinplaces rather than patches

--- helpers/docbook2man-spec.pl
+++ helpers/docbook2man-spec.pl
@@ -7,7 +7,7 @@ docbook2man-spec - convert DocBook RefEntries to Unix manpages
 The SGMLSpm package from CPAN.  This contains the sgmlspl script which
 is used to grok this file.  Use it like this:
 
-nsgmls some-docbook-document.sgml | sgmlspl docbook2man-spec.pl
+onsgmls some-docbook-document.sgml | sgmlspl docbook2man-spec.pl
 
 =head1 DESCRIPTION
 
